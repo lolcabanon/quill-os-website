6d4eff158a8be41f565165ef0d658f3cc910e1dc
lolcabanon <julesses@gmail.com>
2025-04-24 18:49:52 -0400

    Updates

--- _app/immutable/chunks/BbHZH4KU.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1en2fbp)==null?void 0:s.base)??"/quill-os-website";var e;const a=((e=globalThis.__sveltekit_1en2fbp)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/chunks/DcB-pFel.js
@@ -0,0 +1,1 @@
+var s;const l=((s=globalThis.__sveltekit_rgls70)==null?void 0:s.base)??"/quill-os-website";var e;const t=((e=globalThis.__sveltekit_rgls70)==null?void 0:e.assets)??l;export{t as a,l as b};
