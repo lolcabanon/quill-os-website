262f2dc66319d30c1ec576051c1ebf13133158d9
lolcabanon <julesses@gmail.com>
2025-04-24 18:45:26 -0400

    cleanup

--- bottompage.js
+++
@@ -1,8 +0,0 @@
-fetch('bottompage.html')
-.then(res => res.text())
-.then(text => {
-	let oldelem = document.querySelector("script#bottompage");
-	let newelem = document.createElement("div");
-	newelem.innerHTML = text;
-	oldelem.parentNode.replaceChild(newelem,oldelem);
-})
